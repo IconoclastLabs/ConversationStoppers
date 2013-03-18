ef0e1f825d4fb70a03352f4cfff9b6494207a40c
Gant <GantMan@gmail.com>
2013-03-18 13:32:54 -0500

    Implementing needed delegate

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -28,7 +28,7 @@ class RootController < UIViewController
     self.navigationItem.rightBarButtonItem = @nav_bar_button
 
     # Handle ads
-    @ad.currentContentSizeIdentifier = ADBannerContentSizeIdentifierPortrait
+    @ad.delegate = self
 
   end
 
@@ -46,4 +46,7 @@ class RootController < UIViewController
     end
   end
 
+  def bannerView(banner, didFailToReceiveAdWithError:error)
+    p "Needed Banner thingy"
+  end
 end

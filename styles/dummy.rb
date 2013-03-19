2f2937ce37d8304068a645100c42cc1d83263c61
Gant <GantMan@gmail.com>
2013-03-19 10:03:54 -0500

    Updated Dummy file to make Teacup work

--- styles/dummy.rb
+++ styles/dummy.rb
@@ -1,2 +1,9 @@
 # for when I come across one of those silly precompile issues
 # that is not yet supported by teacup
+
+class DummyUIImageView < UIImageView
+private
+  def dummy
+    setUserInteractionEnabled(nil)
+  end
+end
